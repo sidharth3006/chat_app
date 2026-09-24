01df6cb9659e30ff646e1f411ccf461938d1ada0
sidharth3006 <sidharth2020sharma@gmail.com>
2026-09-24 23:01:58 +0530

    Add path

--- frontend/src/context/AppContext.tsx
+++ frontend/src/context/AppContext.tsx
@@ -4,8 +4,8 @@ import Cookies from "js-cookie";
 import axios from "axios";
 import toast, {Toaster} from 'react-hot-toast'
 
-export const user_service = "http://localhost:5000"
-export const chat_service = "http://localhost:5002"
+export const user_service = "http://54.163.43.107:5000"
+export const chat_service = "http://54.163.43.107:5002"
 
 export interface User{
     _id: string;
